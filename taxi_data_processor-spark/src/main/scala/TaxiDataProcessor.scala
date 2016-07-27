e1d0837431b1008ca231f8f178e6a34b16b20f66
Paavo Parkkinen <paavoap@sg.ibm.com>
2016-07-27 16:02:14 +0800

    Linear regression.

--- taxi_data_processor-spark/src/main/scala/TaxiDataProcessor.scala
+++ taxi_data_processor-spark/src/main/scala/TaxiDataProcessor.scala
@@ -1,15 +1,53 @@
 
 import org.apache.spark._
-import org.apache.spark.mllib.classification.{LogisticRegressionWithLBFGS, LogisticRegressionModel}
-import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.linalg.{Vectors, Vector}
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.regression.{LinearRegressionWithSGD, LinearRegressionModel}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.{DataFrame, Column}
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.functions._
 
 object TaxiDataProcessor {
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      println("missing command")
+      System.exit(1)
+    }
+
+    val cmd = args.head
+
+    cmd match {
+      case "local" => runLocal(args.tail)
+      case "bluemix" => runBluemix(args.tail)
+      case _ => println(s"unknown command ${cmd}")
+    }
+
+  }
+
+  // Run on local CSV data
+  def runLocal(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Taxi Data Processor")
+    conf.setMaster("local")
+    val sc = SparkContext.getOrCreate(conf)
+    sc.setLogLevel("WARN")
+    val sqlContext = SQLContext.getOrCreate(sc)
+    processData(loadFromCSV, sqlContext, args)
+    sc.stop()
+  }
+
+  // Run on data from dashDB
+  def runBluemix(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Taxi Data Processor")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    processData(loadFromDashDB, sqlContext, args)
+    sc.stop()
+  }
+
+  // ===========================================
+  // # DATA LOADING
+
   def loadFromDashDB(sqlContext: SQLContext, args: Array[String]) = {
     val host = args(0)
     val user = args(1)
@@ -22,6 +60,7 @@ object TaxiDataProcessor {
         "url" -> url,
         "dbtable" -> table))
   }
+
 
   def loadFromCSV(sqlContext: SQLContext, args: Array[String]) = {
     val path = args(0)
@@ -37,6 +76,9 @@ object TaxiDataProcessor {
       .withColumnRenamed("C2", "LAT")
       .withColumnRenamed("C3", "LNG")
   }
+
+  // ===========================================
+  // # DATA PROCESSING
 
   def minuteofday(c: Column): Column = {
     (hour(c) * 60) + minute(c)
@@ -62,15 +104,15 @@ object TaxiDataProcessor {
     (hGrids * gridLNG) + gridLAT
   }
 
-  val createVector = udf[Vector, Int, Double, Double] { (tod, lat, lng) =>
-    Vectors.dense(tod, lat, lng)
+  val createVector = udf[Vector, Int, Int] { (tod, dow) =>
+    Vectors.dense(tod, dow)
   }
 
   def toLabeledPointRDD(df: DataFrame): RDD[LabeledPoint] = {
     val r_rdd = df.rdd
     r_rdd.map[LabeledPoint] { row =>
-      val fs = row.getAs[Vector](6)
-      val l = row.getDouble(5)
+      val fs = row.getAs[Vector]("features")
+      val l = row.getAs[Long]("label")
       new LabeledPoint(l, fs)
     }
   }
@@ -78,6 +120,10 @@ object TaxiDataProcessor {
   def summary(df: DataFrame) {
     df.printSchema
     df.show(3)
+  }
+
+  def summary(rdd: RDD[LabeledPoint]) {
+    rdd.take(3).foreach(println)
   }
 
   def addTimeAndGridColumns(df: DataFrame): DataFrame = {
@@ -116,67 +162,55 @@ object TaxiDataProcessor {
       .groupBy(selected("timeofday"), selected("dayofweek")).count()
     summary(grouped)
 
-/*
-    val labeled = withTime.withColumn("label", lit(1.0))
-    summary(labeled)
-
-    val vectorized = labeled
-      .withColumn("features", createVector(labeled("timeofday"), labeled("LAT"), df("LNG")))
+    val vectorized = grouped
+      .withColumn("features", createVector(grouped("timeofday"), grouped("dayofweek")))
     summary(vectorized)
 
-    val rdd = toLabeledPointRDD(vectorized)
-    //val sc = sqlContext.sparkContext
-    //val rdd = sc.emptyRDD[LabeledPoint]
+    val labeled = vectorized
+      .withColumnRenamed("count", "label")
+    summary(labeled)
+
+    trainLRModel(labeled)
+  }
+
+  // ===========================================
+  // # MACHINE LEARNING
+
+  def trainLRModel(df: DataFrame) {
+    val rdd = toLabeledPointRDD(df)
+    //summary(rdd)
 
     val splits = rdd.randomSplit(Array(0.8, 0.2))
     val training = splits(0).cache()
     val testing = splits(1)
 
-    val model = new LogisticRegressionWithLBFGS()
-      .setNumClasses(2)
-      .run(training)
+    //val iterationCounts = Array(50, 100, 200, 500)
+    val iterationCounts = Array(50)
+    //val stepSizes = Array(0.00001, 0.000001, 0.0000001, 0.00000001)
+    //val stepSizes = Array(0.00000001, 0.000000001, 0.0000000001, 0.00000000001)
+    val stepSizes = Array(0.00000000001)
+    // Mini batch fraction default value from Spark source
+    val miniBatchFraction = 1.0
+    // Initial weights from a previous run
+    val initialWeights = Vectors.dense(0.6722928916506891,0.005505889905483374)
+    for (iterationCount <- iterationCounts; stepSize <- stepSizes) {
+      println("iterationCount "+iterationCount)
+      println("stepSize "+stepSize)
+      //training.take(3).foreach(println)
+      val model = LinearRegressionWithSGD.train(training,
+        iterationCount, stepSize, miniBatchFraction, initialWeights)
 
-    val pAndLs = testing.map { case LabeledPoint(l, fs) =>
-      val p = model.predict(fs)
-      (p, l)
-    }
+      println("Model intercept "+model.intercept)
+      println("Model weights "+model.weights)
 
-    val ms = new MulticlassMetrics(pAndLs)
-    val p = ms.precision
-    println("Precision: " + p)
-*/
-  }
+      val pAndLs = testing.map { case LabeledPoint(l, fs) =>
+        val p = model.predict(fs)
+        (p, l)
+      }
+      pAndLs.take(3).foreach(println)
 
-  def runLocal(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Taxi Data Processor")
-    conf.setMaster("local")
-    val sc = new SparkContext(conf)
-    sc.setLogLevel("WARN")
-    val sqlContext = new SQLContext(sc)
-    processData(loadFromCSV, sqlContext, args)
-    sc.stop()
-  }
-
-  def runBluemix(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Taxi Data Processor")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
-    processData(loadFromDashDB, sqlContext, args)
-    sc.stop()
-  }
-
-  def main(args: Array[String]) {
-    if (args.length < 1) {
-      println("missing command")
-      System.exit(1)
-    }
-
-    val cmd = args.head
-
-    cmd match {
-      case "local" => runLocal(args.tail)
-      case "bluemix" => runBluemix(args.tail)
-      case _ => println(s"unknown command ${cmd}")
+      val mse = pAndLs.map { case (p, v) => math.pow((v - p), 2)}.mean()
+      println("Mean square error " + mse)
     }
 
   }

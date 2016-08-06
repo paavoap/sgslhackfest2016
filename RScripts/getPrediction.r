12c0870e089828ad488a7678aa234ee89ae8f4b9
mihaotian <mihaotian@gmail.com>
2016-08-06 17:31:20 +0800

    Modify r script to take in Time of day and lat/lon in argument

--- RScripts/getPrediction.r
+++ RScripts/getPrediction.r
@@ -1,8 +1,14 @@
 args <- commandArgs(trailingOnly = TRUE)
-print(args)
+#print(args)
 list<-unlist(strsplit(args,","))
-#read normalised tod(0.0 to 1.0) from argument
-timeOfDay<-as.numeric(list[1])
+
+todList<-unlist(strsplit(list[1]," "))
+
+hhmmss<-todList[5]
+hh<-as.numeric(unlist(strsplit(hhmmss,":"))[1])
+mm<-as.numeric(unlist(strsplit(hhmmss,":"))[2])
+timeOfDay<-(hh+mm/60.0)/24.0
+#print(timeOfDay)
 library("neuralnet")
 load(file = "~/TaxiDataPlotPackage/mymodel.rda")
 test.results <- compute(trainModel,timeOfDay)
